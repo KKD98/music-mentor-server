acdd0fb6d2d0bf36cf0fe26b17f43535b25316ca
KKD98 <KajolKona98@gmail.com>
2023-06-10 23:16:00 +0600

    selected class delete implemented

--- index.js
+++ index.js
@@ -175,6 +175,13 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/myselectedclass/:id' , async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)};
+      const result = await selectedClassesCollection.deleteOne(query);
+      res.send(result);
+    })
+
 
     // -----------------------------------------------
     //           classes related apis end
