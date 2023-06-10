3265813254b3c961391df57ef5e7a7117de8a6e0
KKD98 <KajolKona98@gmail.com>
2023-06-10 12:07:38 +0600

    admin and instructor verified using jwt

--- index.js
+++ index.js
@@ -52,14 +52,12 @@ async function run() {
 
     app.post('/jwt' , (req , res) => {
       const user = req.body;
-      const token = jwt.sign(user , process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: '1h'
-      })
+      const token = jwt.sign(user , process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
       res.send({ token })
     })
 
     // Users related apis start-------------------
-    app.get('/users' , verifyJWT,  async (req , res) => {
+    app.get('/users', verifyJWT,  async (req , res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     })
@@ -75,7 +73,31 @@ async function run() {
       res.send(result);
     })
 
-    
+    app.get('/users/admin/:email',verifyJWT, async(req, res) => {
+      const email = req.params.email;
+
+      if(req.decoded.email !== email){
+        res.send({admin: false})
+      }
+
+      const query = {email: email}
+      const user = await usersCollection.findOne(query);
+      const result = {admin: user?.role === 'admin'};
+      res.send(result);
+    })
+
+    app.get('/users/instructor/:email',verifyJWT, async(req, res) => {
+      const email = req.params.email;
+
+      if(req.decoded.email !== email){
+        res.send({instructor: false})
+      }
+
+      const query = {email: email}
+      const user = await usersCollection.findOne(query);
+      const result = {instructor: user?.role === 'instructor'};
+      res.send(result);
+    })
 
     app.patch('/users/:id' , async(req , res) => {
       const id = req.params.id;
